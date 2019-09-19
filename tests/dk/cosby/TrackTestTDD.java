30549588aba91eda45d854e970f4571f6a2172d4
Cosby1992 <Ande016j@edu.easj.dk>
2019-09-19 16:03:03 +0200

    Test directory setup and first test class (empty). run OK.

---
+++ tests/dk/cosby/TrackTestTDD.java
@@ -0,0 +1,16 @@
+package dk.cosby;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackTestTDD {
+
+    @Test
+    public void testOK(){
+        String s = "TDD";
+        assertEquals("TDD", s);
+    }
+
+
+}
